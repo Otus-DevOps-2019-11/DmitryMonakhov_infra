48924438772b89cfce9a17c0192be4a66c8c1ac1
Dmitry Monakhov <d.a.monakhov@gmail.com>
2019-12-27 22:16:16 +0300

    Fixed variable in create-reddit-vm.sh

--- config-scripts/create-reddit-vm.sh
+++ config-scripts/create-reddit-vm.sh
@@ -12,4 +12,4 @@ gcloud compute instances create reddit-app \
 gcloud compute firewall-rules create default-puma-server \
 --allow tcp:9292 \
 --target-tags=puma-server \
---source-ranges 0.0.0.0/0 \
+--source-ranges 0.0.0.0/0
